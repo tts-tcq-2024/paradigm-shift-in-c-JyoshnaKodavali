bfaf64bb72423fd48cda33db69dce1a8ad478cd8
JyoshnaKodavali <73461390+JyoshnaKodavali@users.noreply.github.com>
2024-06-24 16:43:56 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,38 +1,52 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isTemperatureOutOfRange(float temperature)
-{
-	return (temperature < 0 || temperature > 45); 
-}
-int isSocOutOfRange(float soc)
-{
-	return (soc < 20 || soc > 80);
-}
-int isChargeRateOutOfRange(float chargeRate)
-{
-	return (chargeRate > 0.8);
+typedef enum {
+    OK = 0,
+    TEMPERATURE_OUT_OF_RANGE,
+    SOC_OUT_OF_RANGE,
+    CHARGE_RATE_OUT_OF_RANGE
+} ErrorCode;
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    if (temperature < 0 || temperature > 45) {
+        return TEMPERATURE_OUT_OF_RANGE;
+    }
+    if (soc < 20 || soc > 80) {
+        return SOC_OUT_OF_RANGE;
+    }
+    if (chargeRate > 0.8) {
+        return CHARGE_RATE_OUT_OF_RANGE;
+    }
+    return OK;
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-	if(isTemperatureOutOfRange(temperature)) 
-	{
-		return 0; 
-	} 
-	if(isSocOutOfRange(soc)) 
-	{
-		return 0; 
-	} 
-	if(isChargeRateOutOfRange(chargeRate)) 
-	{
-		return 0; 
-	} 
-	return 1;
+const char *errorMessage(ErrorCode errorCode) {
+    switch (errorCode) {
+        case OK:
+            return "No error";
+        case TEMPERATURE_OUT_OF_RANGE:
+            return "Temperature out of range!";
+        case SOC_OUT_OF_RANGE:
+            return "State of Charge out of range!";
+        case CHARGE_RATE_OUT_OF_RANGE:
+            return "Charge Rate out of range!";
+        default:
+            return "Unknown error";
+    }
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(25, 70, 0.7) == OK);
+    assert(batteryIsOk(50, 85, 0) == TEMPERATURE_OUT_OF_RANGE);
+
+    // Example of printing error messages:
+    printf("For temperature 50, state of charge 85, and charge rate 0:\n");
+    ErrorCode error = batteryIsOk(50, 85, 0);
+    if (error != OK) {
+        printf("Error: %s\n", errorMessage(error));
+    }
+
+    return 0;
 }
 
