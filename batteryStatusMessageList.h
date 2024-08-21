90d13c4b5a5b0a3642d06abb6a4dae8f6be4ff31
JyoshnaKodavali <73461390+JyoshnaKodavali@users.noreply.github.com>
2024-08-21 11:51:56 +0530

    Update and rename batteryStatusMessageList.h to BatteryParameterChecker.c

--- batteryStatusMessageList.h
+++
@@ -1,3 +0,0 @@
-const char *temperatureMessage[] = {"","Temperature out of range!", "Temperatur außerhalb des zulässigen Bereichs!"};
-const char *socMessage[] = {"","State of Charge out of range!", "Ladezustand außerhalb des Bereichs!"};
-const char *chargeRateMessage[] = {"", "Charge Rate out of range!", "Laderate außerhalb des Bereichs!"};
